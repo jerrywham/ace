ef246b16f912957e340f15f68a54dfb9a3d5312e
jerrywham <github@ecyseo.net>
2017-08-21 15:46:36 +0200

    Create fr.php

---
+++ lang/fr.php
@@ -0,0 +1,16 @@
+
+<?php 
+$LANG = array(
+        "L_TITLE"                => "ace",
+        "L_DESCRIPTION"          => "Éditeur web prenant en charge la coloration syntaxique et l'écriture semi-automatique des balises (emmet) à la manière de SublimeText.",
+        "L_ACE_THEME"            => "Thème de l'éditeur",
+        "L_USE_FOR_STATIC_PAGES" => "Utiliser l'éditeur pour les pages statiques ?",
+        "L_USE_FOR_ARTICLES"     => "Utiliser l'éditeur pour les articles ?",
+        "L_USE_FOR_CATEGORIES"   => "Utiliser l'éditeur pour les catégories ?",
+        "L_USE_FOR_COMMENTS"     => "Utiliser l'éditeur pour les commentaires (partie administration) ?",
+        "L_USE_FOR_SETTINGS"     => "Utiliser l'éditeur pour les réglages ?",
+        "L_USE_FOR_CSS"          => "Utiliser l'éditeur pour l'édition des css des plugins ?",
+        "L_KEYBINDING_MENU"      => "Pour afficher le menu : cliquer dans l'éditeur puis ",
+        "L_FONT_SIZE"            => "Taille de la police de l'éditeur (en pixels) ?",
+);
+?>
